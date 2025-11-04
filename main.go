eab0852fc4adbc1408d9fa56938ce717352bac7b
Johnathann <bunleapthay@gmail.com>
2025-11-04 13:03:56 +0700

    crack now

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 
 	"cryptography-lab/utils/crack"
 )
@@ -11,24 +12,54 @@ import (
 func main() {
 	targetHash := "6a85dfd77d9cb35770c9dc6728d73d3f"
 
+	// Open verbose output file (overwrite existing)
+	vfile, err := os.Create("verbose.txt")
+	if err != nil {
+		fmt.Println("Error creating verbose output file:", err)
+		return
+	}
+	defer vfile.Close()
+	vwriter := bufio.NewWriter(vfile)
+	defer vwriter.Flush()
+
 	file, err := os.Open("nord_vpn.txt")
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		msg := fmt.Sprintf("Error opening wordlist file: %v\n", err)
+		fmt.Print(msg)
+		vwriter.WriteString(msg)
 		return
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	found := false
 	for scanner.Scan() {
 		password := scanner.Text()
-		fmt.Printf("Checking: %s\n", password)
+		t := time.Now().Format(time.RFC3339)
+		logLine := fmt.Sprintf("[%s] Checking: %s\n", t, password)
+		// print to stdout and append to verbose file
+		fmt.Print(logLine)
+		vwriter.WriteString(logLine)
+
 		if crack.CheckMD5(password, targetHash) {
-			fmt.Printf("Found: %s\n", password)
+			foundLine := fmt.Sprintf("[%s] Found: %s\n", t, password)
+			fmt.Print(foundLine)
+			vwriter.WriteString(foundLine)
+			vwriter.Flush()
+			found = true
 			break
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
+		msg := fmt.Sprintf("Error reading wordlist: %v\n", err)
+		fmt.Print(msg)
+		vwriter.WriteString(msg)
+	}
+
+	if !found {
+		msg := fmt.Sprintf("[%s] Password not found in wordlist\n", time.Now().Format(time.RFC3339))
+		fmt.Print(msg)
+		vwriter.WriteString(msg)
 	}
 }
